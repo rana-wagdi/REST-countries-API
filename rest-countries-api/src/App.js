16764c83ba379dcfb98c9e72d39b10f6688e9647
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-30 06:19:10 -0700

    add filter component

--- rest-countries-api/src/App.js
+++ rest-countries-api/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import Filter from './components/Filter';
 import Countries from './components/Countries';
 
 function App() {
     return (
       <div className="App">
         <Header />
+        <Filter />
         <Countries />
       </div>
     );
