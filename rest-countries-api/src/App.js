5b4c32cd682ccf2027371b29a30155d86ffd5452
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-30 05:23:51 -0700

    add header

--- rest-countries-api/src/App.js
+++ rest-countries-api/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/Header'
 import Countries from './components/Countries';
 
 function App() {
     return (
       <div className="App">
+        <Header />
         <Countries />
       </div>
     );
--- rest-countries-api/src/components/Countries.js
+++ rest-countries-api/src/components/Countries.js
@@ -26,16 +26,18 @@ const Countries = () => {
                   <article key={numericCode}>
                     <div>
                       <img src={flag} alt={name}></img>
-                      <h3>{name}</h3>
-                      <h4>
-                        Population: <span>{population}</span>
-                      </h4>
-                      <h4>
-                        Region: <span>{region}</span>
-                      </h4>
-                      <h4>
-                        Capital: <span>{capital}</span>
-                      </h4>
+                      <div className="details">
+                        <h3>{name}</h3>
+                        <h4>
+                          Population: <span>{population}</span>
+                        </h4>
+                        <h4>
+                          Region: <span>{region}</span>
+                        </h4>
+                        <h4>
+                          Capital: <span>{capital}</span>
+                        </h4>
+                      </div>
                     </div>
                   </article>
                 );
