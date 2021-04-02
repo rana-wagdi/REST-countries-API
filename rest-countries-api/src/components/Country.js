f671b8d6341e6b57c1de5fb1fee01bb7fba8dade
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-04-02 16:35:48 -0700

    add style

--- rest-countries-api/src/components/Country.js
+++ rest-countries-api/src/components/Country.js
@@ -33,20 +33,45 @@ useEffect(()=> {
                         <div className="country-details">
                           <h2>{name}</h2>
                           <div>
-                            <h5>Native Name: <span> {nativeName}</span></h5>
-                            <h5>Population: <span> {population}</span></h5>
-                            <h5>Region: <span> {region}</span></h5>
-                            <h5>Sub Region: <span> {subregion} </span></h5>
-                            <h5>Capital: <span> {capital}</span> </h5>
+                            <h5>
+                              Native Name: <span> {nativeName}</span>
+                            </h5>
+                            <h5>
+                              Population: <span> {population}</span>
+                            </h5>
+                            <h5>
+                              Region: <span> {region}</span>
+                            </h5>
+                            <h5>
+                              Sub Region: <span> {subregion} </span>
+                            </h5>
+                            <h5>
+                              Capital: <span> {capital}</span>{" "}
+                            </h5>
                           </div>
                           <div>
-                            <h5>Top Level Domain: <span> {topLevelDomain}</span></h5>
-                            <h5>Currencies:<span> {currencies[0].name}</span></h5>
-                            <h5>Languages: <span> {languages[0].name}</span></h5>
+                            <h5>
+                              Top Level Domain: <span> {topLevelDomain}</span>
+                            </h5>
+                            <h5>
+                              Currencies:<span> {currencies[0].name}</span>
+                            </h5>
+                            <h5>
+                              Languages: <span> {languages[0].name}</span>
+                            </h5>
                           </div>
                         </div>
                         <div>
-                            <h3>Border Countries: {borders}</h3>
+                          <h3>Border Countries: </h3>
+                          <div className="borders">
+                            {borders.map((border) => {
+                              return (
+                                <ul key={border}>
+                                  <li>{border}</li>
+                                </ul>
+                              );
+                            })}
+                          </div>
                         </div>
                       </article>
                     );
