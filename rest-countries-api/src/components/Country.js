7af20a3675d860b87df1e1f363b5690e5e1a225f
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-04-02 09:09:09 -0700

    add ink

--- rest-countries-api/src/components/Country.js
+++ rest-countries-api/src/components/Country.js
@@ -1,10 +1,10 @@
 import React from "react";
-import{Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Country = () => {
     return (
         <div>
-            <Link to="/">
+            <Link to="/" className="btn btn-light" >
                 <i className="fas fa-arrow-left"></i>Back Home
             </Link>
             <h1>Country Data</h1>
--- rest-countries-api/src/components/Header.js
+++ rest-countries-api/src/components/Header.js
@@ -7,7 +7,7 @@ const Header= () => {
           <h1>Where in the world!</h1>
         </div>
         <div>
-            <i className ="fas fa-moon"></i>Dark Mode
+          <i className="fas fa-moon"></i> Dark Mode
         </div>
       </header>
     );
