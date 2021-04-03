017f3e9059d4b9416d457a5327adf453d7e82030
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-04-02 20:20:06 -0700

    add style country

--- rest-countries-api/src/components/Country.js
+++ rest-countries-api/src/components/Country.js
@@ -27,38 +27,41 @@ useEffect(()=> {
 
                     return (
                       <article key={numericCode}>
-                        <div className="flag">
-                          <img src={flag} alt={name} />
-                        </div>
-                        <div className="country-details">
-                          <h2>{name}</h2>
-                          <div>
-                            <h5>
-                              Native Name: <span> {nativeName}</span>
-                            </h5>
-                            <h5>
-                              Population: <span> {population}</span>
-                            </h5>
-                            <h5>
-                              Region: <span> {region}</span>
-                            </h5>
-                            <h5>
-                              Sub Region: <span> {subregion} </span>
-                            </h5>
-                            <h5>
-                              Capital: <span> {capital}</span>{" "}
-                            </h5>
+                        <div className="country-inner">
+                          <div className="flag">
+                            <img src={flag} alt={name} />
                           </div>
-                          <div>
-                            <h5>
-                              Top Level Domain: <span> {topLevelDomain}</span>
-                            </h5>
-                            <h5>
-                              Currencies:<span> {currencies[0].name}</span>
-                            </h5>
-                            <h5>
-                              Languages: <span> {languages[0].name}</span>
-                            </h5>
+
+                          <div className="country-details">
+                            <div className="details">
+                              <h2>{name}</h2>
+                              <h5>
+                                Native Name: <span> {nativeName}</span>
+                              </h5>
+                              <h5>
+                                Population: <span> {population}</span>
+                              </h5>
+                              <h5>
+                                Region: <span> {region}</span>
+                              </h5>
+                              <h5>
+                                Sub Region: <span> {subregion} </span>
+                              </h5>
+                              <h5>
+                                Capital: <span> {capital}</span>{" "}
+                              </h5>
+                            </div>
+                            <div className="ab">
+                              <h5>
+                                Top Level Domain: <span> {topLevelDomain}</span>
+                              </h5>
+                              <h5>
+                                Currencies:<span> {currencies[0].name}</span>
+                              </h5>
+                              <h5>
+                                Languages: <span> {languages[0].name}</span>
+                              </h5>
+                            </div>
                           </div>
                         </div>
                         <div>
