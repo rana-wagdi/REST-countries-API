91ec3c7c1de367ca36a34f6b67d0b8e438976305
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-04-03 06:38:27 -0700

    add theme

--- rest-countries-api/src/components/Header.js
+++ rest-countries-api/src/components/Header.js
@@ -3,11 +3,19 @@ import React from "react"
 const Header = () => {
   const changeTheme = () =>{
     const moon = document.querySelector('.fa-moon')
+    const header = document.querySelector(".header");
+    const input = document.querySelector("#search");
+    const select = document.querySelector(".select");
 
   moon.addEventListener('click', () => {
     document.body.classList.toggle('light-theme')
+    header.classList.toggle('light-theme')
+    input.classList.toggle("light-theme")
+    select.classList.toggle("light-theme")
+
+    
   })
-  }
+}
 
 
 
