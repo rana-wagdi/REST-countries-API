cca2858934696c210f96b4ef7b192dac0ca185b8
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-04-03 05:58:20 -0700

    add theme

--- rest-countries-api/src/components/Header.js
+++ rest-countries-api/src/components/Header.js
@@ -1,13 +1,23 @@
 import React from "react"
 
-const Header= () => {
+const Header = () => {
+  const changeTheme = () =>{
+    const moon = document.querySelector('.fa-moon')
+
+  moon.addEventListener('click', () => {
+    document.body.classList.toggle('light-theme')
+  })
+  }
+
+
+
     return (
       <header className="header">
         <div>
           <h1>Where in the world!</h1>
         </div>
         <div>
-          <i className="fas fa-moon"></i> Dark Mode
+          <i className="fas fa-moon" onClick={()=> changeTheme()}></i> 
         </div>
       </header>
     );
